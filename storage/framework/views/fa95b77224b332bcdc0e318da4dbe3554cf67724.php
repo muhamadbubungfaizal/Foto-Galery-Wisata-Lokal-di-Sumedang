f658063b131b489e305ce022b8ef7078e1d769a6
muhamadbubungfaizal <93501748+muhamadbubungfaizal@users.noreply.github.com>
2022-01-10 17:01:41 +0700

    Add files via upload

---
+++ storage/framework/views/fa95b77224b332bcdc0e318da4dbe3554cf67724.php
@@ -0,0 +1,18 @@
+
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://www.tripjalanjalan.com/wp-content/uploads/2018/12/Kolam-Renang-Saung-Cibingbin.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Cibingbin</h5>
+              <p class="card-text"> Saung Cibingbin berada di Desa Citengah Kabupaten Sumedang, letaknya yang tidak terlalu jauh dari pusat kota Sumedang membuat Saung Cibingbin ini ramai pada hari-hari tertentu seperti akhir pekan dan hari libur nasional, Saung Cibingbin jaraknya kira-kira 7-km dari pusat kota dan bisa di jangkau dengan waktu kurang lebih 15-30 menit dari alun-alun Sumedang.</p>
+
+</div>            
+</div>
+</div>
+</div>
+</div>           
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\blog-photo-gallery-wisata-lokal-desa-citengah\resources\views/KampungKaruhun.blade.php ENDPATH**/ ?>
