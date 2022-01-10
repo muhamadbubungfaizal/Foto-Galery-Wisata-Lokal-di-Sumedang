66bc59eba1f3255c3d28acbd9830f7bff9147ba9
muhamadbubungfaizal <93501748+muhamadbubungfaizal@users.noreply.github.com>
2022-01-10 17:00:51 +0700

    Add files via upload

---
+++ views/Index.blade.php
@@ -0,0 +1,44 @@
+@extends('master')
+@section('content')
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://4.bp.blogspot.com/-JcERPYspsHk/V2LptGxSfiI/AAAAAAAACls/4f79BfFibVwbaVs79dN4_Rqd_yAUsQbIACLcB/w1200-h630-p-k-no-nu/Wisata%2BKampung%2BKaruhun%2BCitengah%2BSumedang.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Kampung Karuhun</h5>
+              <p class="card-text"> Kampung Karuhun merupakan sebuah kawasan wisata terpadu yang berada di wilayah Desa Citengah Kecamatan Sumedang Selatan. Lokasinya sekitar tujuh kilometer dari pusat kota Sumedang ke arah selatan. Kawasan Kampung Karuhun berupa pesawahan berbukit dan lingkungan hutan tropis yang masih terjaga keasriannya. Berbagai fasilitas dan wahana wisata tersedia di Kampung Karuhun. Ada fasilitas restoran, meeting room, pojok seni, cottage, saung budaya, meeting room, wedding door serta berbagai wahana seperti atv, flying fox dan river tubbing. Fasilitas restoran Kampung Karuhun menyediakan menu andalan Cobek Karuhun ditambah dengan nasi liwet komplit di buat secara langsung. Tersedia menu lainnya yang bisa dipilih. Ada juga fasilitas Saung Budaya yang digunakan untuk pementasan seni kedaerahan. Bagi yang ingin mencoba olahraga penjelajahan, Kampung Karuhun juga menyediakan wahana adventure.</p>
+            
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://4.bp.blogspot.com/-T_9Ja14gOvA/WQeuiXv7_QI/AAAAAAAAYs0/OJd4gDs64sMMbqOdw5nP4qCzxXKfrheZQCLcB/s1600/cigrobog.jpgg">
+            <div class="card-body">
+              <h5 class="card-title">Curug Gorobog</h5>
+              <p class="card-text"> Curug Gorobog yang berada di sumedang Jawa Barat adalah suatu tempat wisata yang memiliki tiga tingkatan air terjun yang mempesona. rasa sejuk dan tenang langsung kerasa ketika memasuki kompleks wisata Curug Gorobog. Gemercik air yang tenang menciptakan suasana nyaman, sepintas Curug Gorobog tidak terlalu tinggi jika dibandingkan dengan curug lain di Jawa Barat sepeti Curug Cinulang atau Curug Cipongko. Tempat ini nyaman dijadikan tempat berenang dan bermain air.</p>
+
+</div>            
+</div>
+</div>
+</div>
+</div>         
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://sumedangtandang.com/upload/foto/menikmati_suasana_malam.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Cilantung Camp</h5>
+              <p class="card-text"> Cilantung Camp merupakan salah satu lokasi wisata yang berlokasi di perbukitan perkebunan teh Margawindu. Lokasi yang cukup jauh dari pusat keramaian Kampung Cisoka dan kawasan wisata lainnya yang berada di perkebunan teh Margawindu, menyebabkan lokasi ini sedikit tersembunyi. Dari lokasi ini pengunjung bisa menyaksikan matahari terbit dari sebelah timur di pagi hari (sunrise), dan bisa menyaksikan matahari terbenam di sebelah barat ketika sore hari (sunset). Sehingga sangat cocok bagi yang menyukai wisata berkemah atau kemping untuk bermalam di sini. Selain menyajikan kawasan wisata berkemah, Cilantung Camp juga bisa dijadikan lokasi wisata alam dengan hamparan perkebunan teh dan pemandangan alam sekitarnya. Untuk memanjakan pengunjung wisata Cilantung Camp, pengelola juga telah membangun beberapa jenis spot selfie yang berada di kawasan perkebunan teh Margawindu. Beberapa diantaranya merupakan berbentuk menara (saung ranggon) sehingga pengunjung bisa melihat pemandangan yang jauh dari ketinggian atau memotret diri dengan latar belakang hamparan alam sekitar. Ada menara yang berbentuk ayunan, menara tempat duduk dengan lampu di atasnya, ada juga menara dengan dinding bangunan. Di lokasi juga terdapat cafe yang menyediakan menu seperti kopi dan minuman lainnya. Disini juga menyediakan fasilitas toilet.</p>
+</div>            
+</div>
+</div>
+</div>
+</div>           
+@endsection
---
+++ views/KampungKaruhun.blade.php
@@ -0,0 +1,17 @@
+@extends('master')
+@section('content')
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://www.tripjalanjalan.com/wp-content/uploads/2018/12/Kolam-Renang-Saung-Cibingbin.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Cibingbin</h5>
+              <p class="card-text"> Saung Cibingbin berada di Desa Citengah Kabupaten Sumedang, letaknya yang tidak terlalu jauh dari pusat kota Sumedang membuat Saung Cibingbin ini ramai pada hari-hari tertentu seperti akhir pekan dan hari libur nasional, Saung Cibingbin jaraknya kira-kira 7-km dari pusat kota dan bisa di jangkau dengan waktu kurang lebih 15-30 menit dari alun-alun Sumedang.</p>
+
+</div>            
+</div>
+</div>
+</div>
+</div>           
+@endsection
